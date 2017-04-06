f63185c1742f0f822851d044dfd9e2a60c158c21
Diego Paz <pazdiegog@est.frba.utn.edu.ar>
2017-04-06 13:23:26 -0300

    Se agregaron comentarios en el codigo fuente

--- adicion.cpp
+++ adicion.cpp
@@ -10,12 +10,12 @@ DGP
 int main()
 
 {
-    int a, b, suma;
-    std::cout << "Este programa suma dos numeros enteros \n";
+    int a, b, suma;                         /*declaracion de las variables a utilizar en el proceso del programa*/
+    std::cout << "Este programa suma dos numeros enteros \n";       /*Se pide el ingreso de los numeros enteros*/
     std::cout << "Ingrese un numero entero: ";
     std::cin >> a;
     std::cout << "Ingrese otro numero entero: ";
     std::cin >> b;
-    suma= a + b;
-    std::cout << "La suma de "<<a<<" + "<<b<<" es: " <<suma;
+    suma= a + b;                                                    /*"suma" toma el valor de a+b */
+    std::cout << "La suma de "<<a<<" + "<<b<<" es: " <<suma;        /*Se imprime en pantalla el resultado*/
 }
